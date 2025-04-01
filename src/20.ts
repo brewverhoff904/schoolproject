ee04bd18514fb0afaca31e0f5b2dea3204fbd5f4
brewverhoff904 <brewverhoff904@gmail.com>
2025-04-01 16:05:01 +0000

    Add 20.ts

---
+++ src/20.ts
@@ -0,0 +1,17 @@
+class SchoolProject {
+  constructor() {
+    this.name = "Basic GitHub Repository";
+    this.description = "A simple school project made with TypeScript.";
+  }
+
+  // Example method
+  addLesson() {
+    console.log("Adding a new lesson.");
+  }
+}
+
+// Create an instance of the SchoolProject class
+const schoolProject = new SchoolProject();
+
+// Call the method to add a lesson
+schoolProject.addLesson();
